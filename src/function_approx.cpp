7aaf1def52886ac4940f7d58ba0271006ff09ece
Maverobot <quzhengrobot@gmail.com>
2019-09-21 16:38:27 +0200

    code cleanup

--- src/function_approx.cpp
+++ src/function_approx.cpp
@@ -2,46 +2,31 @@
 #include <algorithm>
 #include <random>
 
-std::pair<std::vector<double>, std::vector<double>> getTrainingData(double i_max = 1000000,
-                                                                    double x_min = -M_PI,
-                                                                    double x_max = M_PI) {
-  std::vector<double> x(i_max);
-  std::vector<double> y(i_max);
-
-  size_t i = 0;
-  while (i < i_max) {
-    x[i] = x_min + (x_max - x_min) * i / i_max;
-    y.at(i) = std::cos(x.at(i));
-    i++;
-  }
-
-  return std::make_pair(x, y);
-};
-
 class OneDimMappingDataset : public torch::data::Dataset<OneDimMappingDataset> {
  private:
-  std::vector<double> states_, labels_;
+  size_t size_;
+  double x_min_;
+  double x_max_;
 
  public:
-  explicit OneDimMappingDataset(const std::vector<double>& states,
-                                const std::vector<double>& labels)
-      : states_(states), labels_(labels) {
-    if (states.size() != labels.size()) {
-      throw std::invalid_argument("states and labels must have the same length.");
-    }
-  };
+  explicit OneDimMappingDataset(const size_t size,
+                                const double x_min = -2 * M_PI,
+                                const double x_max = 2 * M_PI)
+      : size_(size), x_min_(x_min), x_max_(x_max){};
   torch::data::Example<> get(size_t index) override {
-    torch::Tensor state = torch::eye(1) * states_.at(index);
-    torch::Tensor label = torch::eye(1) * labels_.at(index);
+    torch::Tensor state = torch::rand(1) * (x_max_ - x_min_) + x_min_;
+    torch::Tensor label = torch::ones(1) * std::cos(state[0].item<double>());
     return {state, label};
   };
 
-  torch::optional<size_t> size() const override { return states_.size(); }
+  torch::optional<size_t> size() const override { return size_; }
 };
 
 int main(int /*argc*/, char* /*argv*/[]) {
-  const size_t kEpochSize = 100;
-  const size_t kBatchSize = 10000;
+  const bool kRestoreFromCheckpoint = true;
+  const std::string kCheckPointFile = "cos-func-approx-checkpoint.pt";
+  const size_t kEpochSize = 1000;
+  const size_t kBatchSize = 1000;
   const int64_t kLogInterval = 10;
   const int64_t kCheckpointEvery = 10000;
 
@@ -52,20 +37,11 @@ int main(int /*argc*/, char* /*argv*/[]) {
     std::cout << "CUDA is available! Training on GPU." << std::endl;
   }
 
-  // Get training data.
-  std::vector<double> train_x;
-  std::vector<double> train_y;
-  std::tie(train_x, train_y) = getTrainingData();
-
-  std::cout << "train_x size: " << train_x.size() << std::endl;
-  std::cout << "train_y size: " << train_y.size() << std::endl;
-
   // Generate a dataset
-  auto data_set = OneDimMappingDataset(train_x, train_y).map(torch::data::transforms::Stack<>());
+  auto data_set = OneDimMappingDataset(100000).map(torch::data::transforms::Stack<>());
 
   const size_t kDataSize = data_set.size().value();
   const int64_t kBatchesPerEpoch = std::ceil(kDataSize / static_cast<double>(kBatchSize));
-  std::cout << "data size: " << kDataSize << std::endl;
 
   // Generate a data loader.
   auto data_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(
@@ -83,9 +59,15 @@ int main(int /*argc*/, char* /*argv*/[]) {
   torch::optim::Adam optimizer(func_approximator->parameters(),
                                torch::optim::AdamOptions(2e-4).beta1(0.5));
 
-  const bool kRestoreFromCheckpoint = false;
   if (kRestoreFromCheckpoint) {
-    torch::load(func_approximator, "cos-func-approx-checkpoint.pt");
+    try {
+      torch::load(func_approximator, kCheckPointFile);
+      std::cout << kCheckPointFile << " loaded. Continue with training on the loaded weights."
+                << std::endl;
+    } catch (const c10::Error e) {
+      std::cout << "Warning: " << e.msg_without_backtrace() << std::endl;
+      std::cout << "Start training from beginning." << std::endl;
+    }
   }
 
   size_t epoch_idx = 0;
@@ -95,14 +77,7 @@ int main(int /*argc*/, char* /*argv*/[]) {
       // Train discriminator with real images.
       func_approximator->zero_grad();
       auto data = batch.data.to(device);
-      // std::cout << data.to(TensorOptions().device(kCPU), false,
-      // true)
-      //           << std::endl;
       auto labels = batch.target.to(device);
-
-      // std::cout << labels.to(TensorOptions().device(kCPU), false,
-      // true)
-      //           << std::endl;
 
       torch::Tensor real_output = func_approximator->forward(data);
       torch::Tensor d_loss_real = torch::mse_loss(real_output, labels);
@@ -110,22 +85,19 @@ int main(int /*argc*/, char* /*argv*/[]) {
       optimizer.step();
 
       if (batch_index % kLogInterval == 0) {
-        std::printf("\r[%2ld/%2ld][%3ld/%3ld] loss: %.4f \n", epoch_idx, kEpochSize, batch_index,
+        std::printf("\r[%2ld/%2ld][%3ld/%3ld] loss: %.6f \n", epoch_idx, kEpochSize, batch_index,
                     kBatchesPerEpoch, d_loss_real.item<double>());
         /*
-        auto test_x = torch::randn(1) * M_PI;
-        auto test_y =
-            func_approximator->forward(test_x.toBackend(c10::Backend::CUDA));
-        std::printf("x = %.5f, target y = %.5f, predicted y = %.5f\n ",
-                    test_x[0].item<double>(),
-                    std::cos(test_x[0].item<double>()),
-                    test_y[0].item<double>());
-        */
+          auto test_x = -2 * M_PI + torch::rand(1) * 4 * M_PI;
+          auto test_y = func_approximator->forward(test_x.toBackend(c10::Backend::CUDA));
+          std::printf("x = %.5f, target y = %.5f, predicted y = %.5f\n ", test_x[0].item<double>(),
+          std::cos(test_x[0].item<double>()), test_y[0].item<double>());
+         */
       }
 
       if (batch_index % kCheckpointEvery == 0) {
         // Checkpoint the model and optimizer state.
-        torch::save(func_approximator, "cos-func-approx-checkpoint.pt");
+        torch::save(func_approximator, kCheckPointFile);
       }
       batch_index++;
     }
