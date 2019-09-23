8f7ccbee05020e674fec937558c86083e7c46a22
Maverobot <quzhengrobot@gmail.com>
2019-09-23 22:07:50 +0200

    error was due to imcompatibility between nn::Sequence and nn::RNN

--- src/time_serie_prediction.cpp
+++ src/time_serie_prediction.cpp
@@ -1,4 +1,7 @@
 #include <torch/torch.h>
+
+// Construct own LSTM model
+
 int main(int /*argc*/, char* /*argv*/[]) {
   // Use GPU when present, CPU otherwise.
   torch::Device device(torch::kCPU);
@@ -7,19 +10,28 @@ int main(int /*argc*/, char* /*argv*/[]) {
     std::cout << "CUDA is available! Training on GPU." << std::endl;
   }
 
-  torch::nn::Sequential time_serie_detector(
-      torch::nn::RNN(torch::nn::RNNOptions(1, 10).dropout(0.2).layers(2).tanh()));
+  const size_t kSequenceLen = 3;
+  const size_t kInputDim = 1;
+  const size_t kHiddenDim = 1;
+  const size_t kOuputDim = 1;
+  auto time_serie_detector = torch::nn::LSTM(torch::nn::LSTMOptions(kInputDim, kHiddenDim)
+                                                 .dropout(0.2)
+                                                 .layers(kSequenceLen)
+                                                 .bidirectional(false));
   time_serie_detector->to(device);
-
   std::cout << time_serie_detector << std::endl;
 
-  auto x = torch::ones(1).toBackend(c10::Backend::CUDA);
-  auto a = torch::ones(10).toBackend(c10::Backend::CUDA);
-  std::cout << "x = " << x << std::endl;
-  std::cout << "a = " << a << std::endl;
+  torch::Tensor input = torch::empty({kSequenceLen, kInputDim});
+  auto input_acc = input.accessor<float, 2>();
+  size_t count = 0;
+  for (float i = 0.1; i < 0.4; i += 0.1) {
+    input_acc[count][0] = i;
+    count++;
+  }
+  input = input.toBackend(c10::Backend::CUDA);
+  std::cout << "input = " << input << std::endl;
   time_serie_detector->zero_grad();
-
-  time_serie_detector->forward(x, a);
+  time_serie_detector->forward(input.toBackend(c10::Backend::CUDA).view({input.size(0), 1, -1}));
 
   return 0;
 }
