812d1144413df3e1703f04fe54e628716a0018bd
Maverobot <quzhengrobot@gmail.com>
2019-09-26 13:05:07 +0200

    cleanup

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -12,10 +12,6 @@ int main() {
   torch::Tensor tensor = torch::eye(3);
   pretty_print("Eye tensor: ", tensor);
 
-  // Create an range tensor
-  tensor = torch::range(1, 9, 1);
-  pretty_print("Range tensor: ", tensor);
-
   // Tensor view is like reshape in numpy, which changes the dimension representation of the tensor
   // without touching its underlying memory structure.
   tensor = torch::range(1, 9, 1);
@@ -27,4 +23,5 @@ int main() {
   pretty_print("Tensor view 3x3x3: ", tensor.view({3, 3, 3}));
   pretty_print("Tensor view 3x3x3 with D0 and D1 transposed: ",
                tensor.view({3, 3, 3}).transpose(0, 1));
+  pretty_print("Tensor view 3x1x9: ", tensor.view({3, 1, -1}));
 }
