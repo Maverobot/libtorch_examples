32fe3d81ef9efdb8c4fff4e9ff82eef3baff7402
Maverobot <quzhengrobot@gmail.com>
2019-10-21 23:13:47 +0200

    added csv -> floor type

--- src/dataset_example.cpp
+++ src/dataset_example.cpp
@@ -1,5 +1,6 @@
 #include <torch/torch.h>
 
+#include <algorithm>
 #include <fstream>
 #include <sstream>
 #include <vector>
@@ -57,6 +58,29 @@ struct SensorData {
   }
 };
 
+struct FloorType {
+  FloorType() = default;
+  FloorType(const std::string& csv_line) {
+    std::istringstream iss(csv_line);
+    series_id = parse_token<decltype(series_id)>(iss);
+    group_id = parse_token<decltype(group_id)>(iss);
+    surface = parse_token<decltype(surface)>(iss);
+  };
+
+  torch::Tensor toTensor(const std::vector<std::string>& surfaces) const {
+    auto iter = std::find(surfaces.cbegin(), surfaces.end(), surface);
+    if (iter == surfaces.end()) {
+      throw std::logic_error("the surfaces must contain the FloorType::surface");
+    }
+    long id = iter - surfaces.begin();
+    return torch::one_hot(torch::tensor({id}, torch::TensorOptions(torch::kLong)), surfaces.size());
+  };
+
+  int series_id;
+  int group_id;
+  std::string surface;
+};
+
 std::ostream& operator<<(std::ostream& o, const SensorData& d) {
   o << "row_id: " << d.row_id << ", series_id: " << d.series_id
     << ", measurement_number: " << d.measurement_number << ", sensor_data: " << d.sensor_data
@@ -66,8 +90,10 @@ std::ostream& operator<<(std::ostream& o, const SensorData& d) {
 
 class CustomDataset : torch::data::Dataset<CustomDataset> {
  public:
-  CustomDataset(const std::string& csv_file_path){
-      // read csv file to sensor_data_ and floor_types_;
+  CustomDataset(const std::string& x_train_csv, const std::string& y_train_csv) {
+    // read csv file to sensor_data_ and floor_types_;
+    auto sensor_data = load_csv_file<SensorData>(x_train_csv);
+    auto floor_types = load_csv_file<FloorType>(y_train_csv);
   };
 
   torch::data::Example<> get(size_t index) override {
@@ -82,7 +108,30 @@ class CustomDataset : torch::data::Dataset<CustomDataset> {
 };
 
 int main(int argc, char* argv[]) {
-  auto data = load_csv_file<SensorData>(argv[1]);
-  std::cout << data << std::endl;
+  /*
+    argv[1] path to X_train.csv
+    argv[2] path to y_train.csv
+   */
+
+  auto x_train_raw = load_csv_file<SensorData>(argv[1]);
+  auto y_train_raw = load_csv_file<FloorType>(argv[2]);
+  std::cout << "sensor data number: " << x_train_raw.size() << std::endl;
+  std::cout << "floor types number: " << y_train_raw.size() << std::endl;
+
+  std::vector<std::string> floor_types;
+  for_each(y_train_raw.cbegin(), y_train_raw.cend(), [&floor_types](const FloorType& d) {
+    if (std::find(floor_types.cbegin(), floor_types.cend(), d.surface) == floor_types.end()) {
+      floor_types.push_back(d.surface);
+    }
+  });
+  std::sort(floor_types.begin(), floor_types.end());
+  std::cout << "floor types: " << floor_types << std::endl;
+
+  std::cout << y_train_raw.at(0).surface << ": " << y_train_raw.at(0).toTensor(floor_types)
+            << std::endl;
+  std::cout << y_train_raw.at(1).surface << ": " << y_train_raw.at(1).toTensor(floor_types)
+            << std::endl;
+  std::cout << y_train_raw.at(4).surface << ": " << y_train_raw.at(4).toTensor(floor_types)
+            << std::endl;
   return 0;
 }
